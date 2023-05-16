bafc3b7f40da0abc12023b6c8f21bdcfcfa3ba5e
NUWANI SUDARSHIKA <nuwanisudarshi@gmail.com>
2023-05-16 15:30:15 +0530

    Encapsulation practical code

---
+++ src/BankAccount.java
@@ -0,0 +1,47 @@
+public class BankAccount {
+    private long acc_no;
+    private String name,email;
+    private double amount;
+
+    public long getAcc_no() {
+        return acc_no;
+    }
+
+    public void setAcc_no(long acc_no) {
+        this.acc_no = acc_no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
+class Run{
+    public static void main(String[] args) {
+        BankAccount ba= new BankAccount();
+        ba.setAcc_no(1234567890);
+        ba.setName("Gawesh Prabashwara");
+        ba.setEmail("gawesh2020java@gmail.com");
+        ba.setAmount(24567.987);
+        System.out.println(ba.getAcc_no()+" "+ba.getName()+" "+ba.getEmail()+" "+ba.getAmount());
+    }
+}
---
+++ src/Employee.java
@@ -0,0 +1,51 @@
+public class Employee {
+    private int emp_id;
+    private String emp_name;
+    private int emp_age;
+    private double emp_salary;
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public String getEmp_name() {
+        return emp_name;
+    }
+
+    public void setEmp_name(String emp_name) {
+        this.emp_name = emp_name;
+    }
+
+    public int getEmp_age() {
+        return emp_age;
+    }
+
+    public void setEmp_age(int emp_age) {
+        this.emp_age = emp_age;
+    }
+
+    public double getEmp_salary() {
+        return emp_salary;
+    }
+
+    public void setEmp_salary(double emp_salary) {
+        this.emp_salary = emp_salary;
+    }
+}
+class RunEmployee{
+    public static void main(String[] args) {
+        Employee e=new Employee();
+        e.setEmp_id(1236789123);
+        e.setEmp_name("Gawesh");
+        e.setEmp_age(25);
+        e.setEmp_salary(567890.987);
+        System.out.println(e.getEmp_id());
+        System.out.println(e.getEmp_name());
+        System.out.println(e.getEmp_age());
+        System.out.println(e.getEmp_salary());
+    }
+}
